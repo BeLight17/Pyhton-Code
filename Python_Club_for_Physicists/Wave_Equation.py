110b0eb1f21ba535c66f72c1753e8f5c73e9770c
BeLight17 <57470517+BeLight17@users.noreply.github.com>
2020-03-08 20:32:59 -0500

    Add files via upload
    Python Club for Physicists 2020

---
+++ Python_Club_for_Physicists/Wave_Equation.py
@@ -0,0 +1,64 @@
+"""
+Ecuacion de onda en una cuerda
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
+#%%
+# Datos
+
+L = 1 #longitud de la cuerda
+T = 40 #tension
+mu = 0.01 #densidad lineal de la cuerda
+v = (T/mu)**0.5 #velocidad de propagacion
+v_prim = v #CFL: v<=v'=delta(x)/delta(t)
+
+#%%
+# Desplazamiento vertical
+y = np.zeros((100,1000)) # y[filas=x][columnas=t]
+
+#Condiciones iniciales
+for i in range(0,81): y[i,0] = 0.00125*i
+for i in range(81,100): y[i,0] = 0.1 - 0.005*(i-80)
+
+#Algoritmo
+for i in range(1,99): 
+    y[i,1] = 2*y[i,0]-y[i,0] + 0.5*(v/v_prim)**2*(y[i+1,0]+y[i-1,0]-2*y[i,0])
+for i in range(1,99):
+    y[i,2] = 2*y[i,1]-y[i,1] + 0.5*(v/v_prim)**2*(y[i+1,1]+y[i-1,1]-2*y[i,1])
+for i in range(1,99):
+    for j in range(2,999):
+        y[i,j+1] = 2*y[i,j]-y[i,j-1] + 0.5*(v/v_prim)**2*(y[i+1,j]+y[i-1,j]-2*y[i,j])
+    
+#%%
+# Plot 
+
+x_plot = np.linspace(0,1,100)
+
+for i in range(0,10):
+    y_plot = y[:,i]
+    plt.plot(x_plot,y_plot)
+    
+#%%
+#Animation
+    
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 1), ylim=(-0.5, 0.5))
+line, = ax.plot([], [], lw=1)
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def animate(i):
+    x_an = np.linspace(0,1,100)
+    y_an = y[:,i]
+    line.set_data(x_an, y_an)
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=200, interval=10, blit=True)
+
+anim.save('string.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
